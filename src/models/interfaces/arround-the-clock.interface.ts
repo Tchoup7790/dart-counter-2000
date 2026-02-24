6dbf0aa079b23b0d8ff739a0522b7d69c8261459
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2026-02-24 22:13:08 +0100

    Modification interface ATC

--- src/models/interfaces/arround-the-clock.interface.ts
+++ src/models/interfaces/arround-the-clock.interface.ts
@@ -1,7 +1,11 @@
+import type { DartThrow } from './dart-throw.interface'
+
 export interface AtcOptions {
   mustDouble: boolean
 }
 
-export interface AtcRoundResult {
-  winner: number | undefined
+export interface RoundResultAtc {
+  winner: boolean
+  validThrows: DartThrow[]
+  score: number
 }
